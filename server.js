fac143b991753ad1d3ba2751b76ef3253f394385
Sathya <sathyajai1203@gmail.com>
2026-04-06 20:07:44 +0530

    fix backend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+import cors from "cors";
+
+app.use(cors());
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
